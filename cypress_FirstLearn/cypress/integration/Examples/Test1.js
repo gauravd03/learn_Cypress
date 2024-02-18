6de71cd18cd1d652997bd2e8484c765a6f9172ab
gauravd03 <gaurauvdandekar0071@gmail.com>
2024-02-18 21:56:56 +0530

    cypress learnings and code changes

---
+++ cypress_FirstLearn/cypress/integration/Examples/Test1.js
@@ -0,0 +1,14 @@
+describe( 'my first test suite', function(){
+
+it('my first test case', function(){
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+  
+    cy.get('.search-keyword').type('ca');
+    cy.wait(2000);
+   
+    cy.get('.product:visible').should('have.length',4);
+})
+
+
+
+})
